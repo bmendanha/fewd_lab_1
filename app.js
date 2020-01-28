a9ce8df194ad53cf68a4a28b90527511c75d8296
bmendanha <bmendanha@hotmail.com>
2020-01-28 19:16:22 +0000

    ‘commit’

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ x=z;
 console.log("X value="+x+" Y value="+y);
 
 var firstName="Bruno";
-var favouriteColor;
+var favouriteColor="Blue";
 var favouriteFood="any food";
 var city="Dublin";
-// show the value of 'firstname', 'favouritecolor','favouritefood' and 'city'
+// show the value of 'firstName', 'favouriteColor','favouriteFood' and 'city'
 console.log("Name: "+ firstName + " Favourite color: "+ favouriteColor + " Favourite food: "+ favouriteFood+" City: "+ city);
 
 var phrase= "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
 var endingPhrase= "is what we see. Rainbow colours, are made with light bending you see!";
-var one = "Red"; 
-var two = "orange";
-var three = "yellow";
-var four = "green";
-var five = "blue";
-var six = "indigo";
-var seven = "violet";
+var one = "Red "; 
+var two = "orange ";
+var three = "yellow ";
+var four = "green ";
+var five = "blue ";
+var six = "indigo ";
+var seven = "violet ";
 
-console.log(phrase+" "+one+two+three+four+five+six+seven+endingPhrase)
+console.log(phrase+" "+one+ " "+two+ " "+three+ " "+four+" "+five+" "+six+" "+seven+" "+endingPhrase);
 
