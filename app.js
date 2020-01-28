454d61df0d9e4c5b8d321c2fd2af10bd6a28c1bb
bmendanha <bmendanha@hotmail.com>
2020-01-28 21:09:08 +0000

    ‘commit’

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // Bruno H. M. Mendanha - Id: 20325
-var x='Laurel';
+var a='TIC'; //Exercise 6
+var a='TAC';
+var a='TOE';
+console.log(a);
+
+var x='Laurel'; // Exercise 7
 var y='Hardy';
 var z=y;
 y=x;
@@ -7,13 +12,14 @@ x=z;
 // change the value of 'x' by 'y' and the value of 'y' by 'x'
 console.log("X value="+x+" Y value="+y);
 
-var firstName="Bruno";
+var firstName="Bruno"; // Exercise 8
 var favouriteColor="Blue";
 var favouriteFood="any food";
 var city="Dublin";
-// show the value of 'firstName', 'favouriteColor','favouriteFood' and 'city'
+// show the value of 'firstName', 'favouriteColor','favouriteFood' and 'city' (Exercise 9)
 console.log("Name: "+ firstName + " Favourite color: "+ favouriteColor + " Favourite food: "+ favouriteFood+" City: "+ city);
 
+// Exercise 10
 var phrase= "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
 var endingPhrase= "is what we see. Rainbow colours, are made with light bending you see!";
 var one = "Red "; 
@@ -23,6 +29,6 @@ var four = "green ";
 var five = "blue ";
 var six = "indigo ";
 var seven = "violet ";
-
+// Exercise 11
 console.log(phrase+" "+one+ " "+two+ " "+three+ " "+four+" "+five+" "+six+" "+seven+" "+endingPhrase);
 
