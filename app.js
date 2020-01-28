205712ca291796bcb1d5c132ec1279bcfffa7916
bmendanha <bmendanha@hotmail.com>
2020-01-28 15:57:19 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var x='Laurel';
+var y='Hardy';
+var z=y;
+y=x;
+x=z;
+// change the value of 'x' by 'y' and the value of 'y' by 'x'
+console.log("X value="+x+" Y value="+y);
+
+var firstName="Bruno";
+var favouriteColor;
+var favouriteFood="any food";
+var city="Dublin";
+// show the value of 'firstname', 'favouritecolor','favouritefood' and 'city'
+console.log("Name: "+ firstName + " Favourite color: "+ favouriteColor + " Favourite food: "+ favouriteFood+" City: "+ city);
+
+var phrase= "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase= "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red"; 
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+console.log(phrase+" "+one+two+three+four+five+six+seven+endingPhrase)
+
